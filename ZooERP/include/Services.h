d0ac724098ae8e6f049b47413bf3d8445d16a1bf
urapri08 <urapri08@gmail.com>
2026-04-09 17:34:27 +0300

    Доработка
    Доработал 1 домашку

--- ZooERP/include/Services.h
+++ ZooERP/include/Services.h
@@ -3,84 +3,76 @@
 #include "Domain.h"
 #include <vector>
 #include <iostream>
+#include <memory>
 
 class VetClinic : public IVetClinic {
 public:
-  bool checkHealth(const Animal& animal) override {
-    return animal.isHealthy();
-  }
+    bool checkHealth(const Animal& animal) override {
+        return animal.isHealthy();
+    }
 };
 
 class IZoo : public IService {
 public:
-  virtual void addAnimal(Animal* animal) = 0;
-  virtual void addThing(Thing* thing) = 0;
-  virtual int getTotalFood() const = 0;
-  virtual void printContactZoo() const = 0;
-  virtual void printInventory() const = 0;
+    virtual void addAnimal(std::shared_ptr<Animal> animal) = 0;
+    virtual void addThing(std::shared_ptr<Thing> thing) = 0;
+    virtual int getTotalFood() const = 0;
+    virtual void printContactZoo() const = 0;
+    virtual void printInventory() const = 0;
 };
 
 class Zoo : public IZoo {
 private:
-  IVetClinic* clinic;
-  std::vector<Animal*> animals;
-  std::vector<Thing*> things;
+    std::shared_ptr<IVetClinic> clinic;
+    std::vector<std::shared_ptr<Animal>> animals;
+    std::vector<std::shared_ptr<Thing>> things;
 
 public:
-  Zoo(IVetClinic* c) : clinic(c) {}
-  ~Zoo() {
-    for (auto a : animals) {
-      delete a;
-    }
-    for (auto t : things) {
-      delete t;
-    }
-  }
+    Zoo(std::shared_ptr<IVetClinic> c) : clinic(c) {}
 
-  void addAnimal(Animal* animal) override {
-    if (!animal) return;
-
-    if (clinic->checkHealth(*animal)) {
-      animals.push_back(animal);
-      std::cout << "[+] " << animal->getName() << " успешно прошел медосмотр и добавлен в зоопарк.\n";
-    }
-    else {
-      std::cout << "[-] " << animal->getName() << " болен Клиника его не пропустила.\n";
-      delete animal;
-    }
-  }
-
-  void addThing(Thing* thing) override {
-    if (!thing) return;
-    things.push_back(thing);
-  }
-
-  int getTotalFood() const override {
-    int total_food = 0;
-    for (const auto a : animals) {
-      total_food += a->getFood();
-    }
-    return total_food;
-  }
-
-  void printContactZoo() const override {
-    std::cout << "\n Животные для контактного зоопарка\n";
-    for (const auto a : animals) {
-      if (auto player = dynamic_cast<IPlayerAnimal*>(a)) {
-        if (player->canPlay()) {
-          std::cout << a->getName() << " (Инв. номер: " << a->getNumber() << ")\n";
+    void addAnimal(std::shared_ptr<Animal> animal) override {
+        if (!animal) return;
+        
+        if (clinic->checkHealth(*animal)) {
+            animals.push_back(animal);
+            std::cout << "[+] " << animal->getName() << " прошел медосмотр и добавлен в зоопарк.\n";
+        } else {
+            std::cout << "[-] " << animal->getName() << " болен! Клиника его не пропустила.\n";
         }
-      }
     }
-  }
 
-  void printInventory() const override {
-    std::cout << "\nИнвкнтаризация Зоопарка\n";
-    for (const auto a : animals) {
-      std::cout << "Животное: " << a->getName() << " [Номер:" << a->getNumber() << "]\n";
+    void addThing(std::shared_ptr<Thing> thing) override {
+        if (!thing) return;
+        things.push_back(thing);
+        std::cout << "[+] " << thing->getName() << " добавлен в инвентарь.\n";
     }
-    for (const auto t : things) {
-      std::cout << "Вещь: " << t->getName() << " [Номер:" << t->getNumber() << "]\n";
+
+    int getTotalFood() const override {
+        int total_food = 0;
+        for (const auto& a : animals) {
+            total_food += a->getFood();
+        }
+        return total_food;
     }
-  }
+
+    void printContactZoo() const override {
+        std::cout << "\n--- Животные для контактного зоопарка ---\n";
+        for (const auto& a : animals) {
+            if (auto player = std::dynamic_pointer_cast<IPlayerAnimal>(a)) {
+                if (player->canPlay()) {
+                    std::cout << a->getName() << " (Инв. номер: " << a->getNumber() << ")\n";
+                }
+            }
+        }
+    }
+
+    void printInventory() const override {
+        std::cout << "\n--- Инвентаризация ---\n";
+        for (const auto& a : animals) {
+            std::cout << "Животное: " << a->getName() << " [Номер: " << a->getNumber() << "]\n";
+        }
+        for (const auto& t : things) {
+            std::cout << "Вещь: " << t->getName() << " [Номер: " << t->getNumber() << "]\n";
+        }
+    }
 };
